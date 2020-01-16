08db2cf1598a4812de3e537fd1bff61a3c130555
ADIL ABDULLAH KHAN <36574088+adil1806@users.noreply.github.com>
2020-01-16 16:29:33 +0530

    pp

--- demo1.java
+++ demo1.java
@@ -1,6 +1,6 @@
 class Simple{  
     public static void main(String args[]){  
      System.out.println("Hello Java adil");
-        Sysytem.out.println("narang");
+        System.out.println("narang");
     }  
 }  

58652744d2f680204d8b8c6addbaddfda38d1efd
ADIL ABDULLAH KHAN <36574088+adil1806@users.noreply.github.com>
2020-01-18 16:18:34 +0530

    today

--- demo1.java
+++ demo1.java
@@ -2,5 +2,6 @@ class Simple{
     public static void main(String args[]){  
      System.out.println("Hello Java adil khan");
         System.out.println("narang");
+        System.out.println("khan");
     }  
 }  
